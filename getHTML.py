714f1c76515b21c60cbfdff4519907efe254c551
perlcloud <cloud.perl@gmail.com>
2019-02-24 00:49:19 -0500

    Redue all the things

---
+++ getHTML.py
@@ -0,0 +1,32 @@
+"""
+Gets the html of a url using headless chrome
+and returns html and download time.
+"""
+
+import time
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+
+def get_html(url):
+    start = time.time()
+
+    # create a new Chrome session
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.implicitly_wait(30)
+    driver.get(url)
+
+    # Selenium hands the page source to Beautiful Soup
+    html = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+
+    end = time.time()
+    get_html_time = start - end
+    return html, get_html_time
